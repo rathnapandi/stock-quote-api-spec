1f072a26c4609879f5464877e41a1192e1264780
adorairajan@axway.com <adorairajan@axway.com>
2024-04-24 10:15:55 -0500

    Modified 18 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0NDI4Nw/spectral.js?branch_slug=main&token=b48008fb-de5b-461f-a5cc-859bfb7fd9d6";
+export default { extends: ruleset };
